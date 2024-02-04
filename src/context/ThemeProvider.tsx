64642b16df916c89718ba49b500b18dc7b0b8e14
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-02-03 18:03:40 -0600

    Creating project carpets

---
+++ src/context/ThemeProvider.tsx
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react"
+
+const keyString = 'data-theme'
+
+type TThemeContext = {
+    theme: string
+    setTheme: (theme: string) => void
+}
+
+export const ThemeContext = createContext({} as TThemeContext)
+
+export const useTheme = () => {
+    if (!ThemeContext) throw new Error('useTheme must be used within a ThemeProvider');
+
+    return ThemeContext
+}
+
+export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const defaultTheme = localStorage.getItem(keyString) || 'light'
+    const [theme, _setTheme] = useState(defaultTheme)
+    document.documentElement.setAttribute(keyString, defaultTheme);
+
+    const setTheme = (theme: string) => {
+        localStorage.setItem(keyString, theme);
+        document.documentElement.setAttribute(keyString, theme);
+        _setTheme(theme);
+    }
+
+    return (
+        <ThemeContext.Provider value={{
+            theme,
+            setTheme
+        }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
