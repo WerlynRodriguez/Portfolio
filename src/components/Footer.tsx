c9e2cd436d84a05ed79805a7b9f7b336ace7a179
Werlyn Rodriguez <rdwerlynjose.16@gmail.com>
2024-04-25 13:43:50 -0600

    Refactor Footer component to remove unnecessary code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,76 +4,74 @@ import { TSections } from "../types";
 import SimpleIcon from "./SimpleIcon";
 
 interface IFooterProps {
-    sections?: TSections;
+  sections?: TSections;
 }
 
 /**
  * Generic footer used in overviews pages.
  */
 export default function (props: IFooterProps) {
-    const { t } = useTranslation('footer');
-    const { 
-        sections = {}
-    } = props;
+  const { t } = useTranslation("footer");
+  const { sections = {} } = props;
 
-    const socials = [
-        {
-            icon: "Github",
-            link: "https://github.com/WerlynRodriguez"
-        },
-        {
-            icon: "Linkedin",
-            link: "https://www.linkedin.com/in/werlyn-rodriguez-760007183/"
-        }
-    ]
+  const socials = [
+    {
+      icon: "Github",
+      link: "https://github.com/WerlynRodriguez",
+    },
+    {
+      icon: "Linkedin",
+      link: "https://www.linkedin.com/in/werlyn-rodriguez-760007183/",
+    },
+  ];
 
-    const Social = ({
-        icon,
-        link
-    }: {
-        icon: string;
-        link: string;
-    }) => {
-        const iconInfo = getIcon(icon);
-
-        return (
-            <a className="btn btn-neutral" href={link} target="_blank">
-                <SimpleIcon className="w-6 h-6" name={icon} showTitle/>
+  return (
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content">
+      {sections && (
+        <nav className="grid grid-flow-col gap-4">
+          {Object.keys(sections).map((nameSec) => (
+            <a
+              className="link link-hover"
+              href={`#${sections[nameSec].key}`}
+              key={sections[nameSec].key}
+            >
+              {sections[nameSec].label}
             </a>
-        )
-    }
+          ))}
+        </nav>
+      )}
 
-    return (
-        <footer className="footer footer-center p-10 bg-base-200 text-base-content">
-            {sections &&
-            <nav className="grid grid-flow-col gap-4">
-                {Object.keys(sections).map(nameSec =>
-                    <a className="link link-hover" href={`#${sections[nameSec].key}`} key={sections[nameSec].key}>
-                        {sections[nameSec].label}
-                    </a>
-                )}
-            </nav>}
+      <nav>
+        <div className="grid grid-flow-col gap-4">
+          {socials.map((social, i) => (
+            <a className="btn btn-neutral" href={social.link} target="_blank">
+              <SimpleIcon className="w-6 h-6" name={social.icon} showTitle />
+            </a>
+          ))}
+        </div>
+      </nav>
 
-            <nav>
-                <div className="grid grid-flow-col gap-4">
-                    {socials.map((social, i) =>
-                        <Social
-                            key={i}
-                            {...social}
-                        />
-                    )}
-                </div>
-            </nav>
-
-            <aside>
-                <p>
-                    2024 - {t('designedBy')} <a className="link" href="https://github.com/WerlynRodriguez" target="_blank">Werlyn</a> ♥
-                    <br/>
-                    <a className="link" href="https://github.com/WerlynRodriguez/Portfolio" target="_blank"> 
-                        {t('pageCode')} 
-                    </a>
-                </p>
-            </aside>
-        </footer>
-    )
+      <aside>
+        <p>
+          2024 - {t("designedBy")}{" "}
+          <a
+            className="link"
+            href="https://github.com/WerlynRodriguez"
+            target="_blank"
+          >
+            Werlyn
+          </a>{" "}
+          ♥
+          <br />
+          <a
+            className="link"
+            href="https://github.com/WerlynRodriguez/Portfolio"
+            target="_blank"
+          >
+            {t("pageCode")}
+          </a>
+        </p>
+      </aside>
+    </footer>
+  );
 }
